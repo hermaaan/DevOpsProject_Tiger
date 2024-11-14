9a782310d8f654f6d66df57fd68cdf06aba7752b
Herman <147034384+hermaaan@users.noreply.github.com>
2024-11-14 13:58:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -73,6 +73,4 @@ def update_weather():
 
 # Running the Flask App
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
-
+    app.run(host="0.0.0.0", port=5000, debug=True)
